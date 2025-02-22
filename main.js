ace834d28024ead01253e808fd498e4e3a0891ae
Linda Giang <97063743+lattematics@users.noreply.github.com>
2025-02-21 18:20:13 -0800

    Transforming data

--- main.js
+++ main.js
@@ -24,6 +24,38 @@ const svg2_RENAME = d3.select("#lineChart2")
 // 2.a: LOAD...
 d3.csv("weather.csv").then(data => {
     // 2.b: ... AND TRANSFORM DATA
+    data.forEach(d => {
+        d.date = new Date(d.date);
+        //might consider consolodating the data and mapping by month-year instead of daily
+            //d.year = new Date(d.date).getFullYear(); // get year
+            //d.month = new Date(d.date).getMonth() + 1; // get month
+        d.actualMax = +d.actual_max_temp;
+        d.avgMax = +d.average_max_temp;
+        d.recordMax = +d.record_max_temp;
+    });
+    console.log("Raw data:", data);
+
+
+    const filteredData = data.filter(d => d.city_full === "Indianapolis, IN");
+    console.log("Filtered data 1:", filteredData);
+
+
+    const groupedData = d3.groups(filteredData, d => d.date)
+    .map(([date, entries]) => ({
+        date,
+        actualMax: d3.mean(entries, e => e.actualMax),
+        avgMax: d3.mean(entries, e => e.avgMax),
+        recordMax: d3.mean(entries, e => e.recordMax)
+    }));
+    console.log("Grouped data:", groupedData);
+
+
+    const pivotedData = groupedData.flatMap(({ date, actualMax, avgMax, recordMax }) => [
+        { date, maxTemperature: actualMax, measurement: "Actual" },
+        { date, maxTemperature: avgMax, measurement: "Average" },
+        { date, maxTemperature: recordMax, measurement: "Record" }
+    ]);
+    console.log("Pivoted data:", pivotedData);
 
     // 3.a: SET SCALES FOR CHART 1
 
